0cdd50f9dc34761352a3ea2b44e4571d7ebb6137
Stephen Scaff <smscaff@gmail.com>
2022-04-19 11:06:09 -0700

    Updates

---
+++ index.f4e4271d.js
@@ -0,0 +1,2 @@
+if(document.querySelector("#cursor")){AnimatedCursor().init()}if(document.querySelector("#cursor-donut")){AnimatedCursor({cursorInnerSelector:"#donut-cursor-inner",cursorOuterSelector:"#donut-cursor-outer",color:"#0ff",outerAlpha:.1,outerBorderSize:3,size:{inner:8,outer:38},hoverScale:{inner:.5,outer:1.4},clickScale:{inner:1.4,outer:.1}}).init()}
+//# sourceMappingURL=index.f4e4271d.js.map
